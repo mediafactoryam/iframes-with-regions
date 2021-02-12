042a5f77c239cdc04d5f2d7197d4dff741a29097
sandaramet <aram@gratun.am>
2021-02-12 16:51:07 +0400

    get parent parent page width and update iframe size

--- illegal-buildings/script.js
+++ illegal-buildings/script.js
@@ -2,33 +2,64 @@ const DEFAULT_SLUG = 'kentron'
 
 $(document).ready(function ($) {
     const textsPlace = $("#texts");
-    $(window.parent).resize(function(){
-        console.log(this.innerWidth) 
 
+    const PARENT_HEIGHT = getPareHeight()
+    const MOBILE_HEIGHT = 1000
+    const DESKTOP_HEIGHT = 600
+    const CONTAINER_MOBILE_CLASS = 'mobile_container'
+    updateIframe(
+        PARENT_HEIGHT,
+        MOBILE_HEIGHT,
+        DESKTOP_HEIGHT,
+        CONTAINER_MOBILE_CLASS
+    )
+    $(window.parent).resize(function () {
+        updateIframe(
+            this.innerWidth,
+            MOBILE_HEIGHT,
+            DESKTOP_HEIGHT,
+            CONTAINER_MOBILE_CLASS
+        )
     })
     updateNumbers(data)
     updateTexts(DEFAULT_SLUG)
-    
-    $(".region").click(function(e){
+
+    $(".region").click(function (e) {
         let slug = $(this).attr("id")
         updateTexts(slug)
     })
-    
+
     function updateNumbers(data) {
         for (let reg in data) {
-            $("#" + reg + " .number").text(data[reg].texts.length!=0 ? data[reg].texts.length : "")
+            $("#" + reg + " .number").text(data[reg].texts.length != 0 ? data[reg].texts.length : "")
         }
     }
 
 
-    function updateTexts(slug){
+    function updateTexts(slug) {
         $("#title").text(data[slug].title)
         textsPlace.text("")
-        for(let txt of data[slug].texts){
-            textsPlace.append("<p>"+txt.text+"</p>");
+        for (let txt of data[slug].texts) {
+            textsPlace.append("<p>" + txt.text + "</p>");
         }
 
     }
+    function updateIframe(documentWidth, MOBILE_HEIGHT, DESKTOP_HEIGHT, CONTAINER_MOBILE_CLASS) {
+        if (documentWidth < 900) {
+            changeIframePareHeight(MOBILE_HEIGHT)
+            $('.container').addClass(CONTAINER_MOBILE_CLASS)
+        } else {
+            $('.container').removeClass(CONTAINER_MOBILE_CLASS)
+            changeIframePareHeight(DESKTOP_HEIGHT)
+        }
+    }
+    function changeIframePareHeight(height) {
+        window.parent.document.getElementsByClassName('map_iframe')[0].style.height = height
+    }
+    function getPareHeight() {
+        return window.parent.document.getElementsByClassName('map_iframe')[0].style.height
+    }
+
 });
 
 
